82d1fce1e7c0de119a9f7604976bfb41403e7f10
Richard Fauli <Richard Fauli>
2016-10-04 16:36:40 +0200

    cout something to see how particle.h works

--- Integrators/eulercromer.cpp
+++ Integrators/eulercromer.cpp
@@ -1,5 +1,6 @@
 #include "eulercromer.h"
 #include "../system.h"
+#include <iostream>
 
 EulerCromer::EulerCromer(System* system)
     : Integrator(system) {
@@ -9,6 +10,8 @@ void EulerCromer::integrateOneStep(std::vector<Particle*> particles) {
     m_system->computeForces();
     for (int i=0; i<particles.size(); i++) {
         Particle *p = particles.at(i);
+        //std::cout << p->getVelocity() << std::endl; // check to see what prints
+
         /*
          * This is where you need to update the positions and the velocities
          * and the velocities of each particle according to the Euler-Cromer
