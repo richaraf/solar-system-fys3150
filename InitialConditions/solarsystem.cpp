125497e01a7a0a27212b94311bdf3eb1aa8b8448
Richard Fauli <Richard Fauli>
2016-10-13 12:32:41 +0200

    added solarsystem files

---
+++ InitialConditions/solarsystem.cpp
@@ -0,0 +1,6 @@
+#include "solarsystem.h"
+
+solarsystem::solarsystem()
+{
+
+}
---
+++ InitialConditions/solarsystem.h
@@ -0,0 +1,11 @@
+#ifndef SOLARSYSTEM_H
+#define SOLARSYSTEM_H
+
+
+class solarsystem
+{
+public:
+    solarsystem();
+};
+
+#endif // SOLARSYSTEM_H
